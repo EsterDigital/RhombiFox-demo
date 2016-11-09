1b816f84d7d0423fab65a7409bf6bae75a71a66e
borsch.watcher <borsch.watcher@gmail.com>
2016-11-09 17:00:30 +0200

    Add videos list

--- app/smart-tv/scenes/home/home.js
+++ app/smart-tv/scenes/home/home.js
@@ -2,14 +2,36 @@ goog.provide('SmartTv.scenes.Home');
 goog.require('zb.layers.CuteScene');
 goog.require('SmartTv.scenes.templates.home.home');
 
-
+goog.require('zb.ui.BaseList');
+goog.require('zb.xhr.simple');
 
 /**
  * @constructor
  * @extends {zb.layers.CuteScene}
  */
 SmartTv.scenes.Home = function() {
-	goog.base(this);
+  goog.base(this);
+
+  var self = this;
+
+  zb.xhr.simple.send('GET', 'http://webcaster.pro/api/admin/events.json')
+    .then(function(xhr) {
+      var items = JSON.parse(xhr.responseText);
+
+      console.log(items.length);
+
+      var res = [];
+      for(var i = 0, length = items.length; i < length; i++) {
+        res.push(items[i].name);
+      }
+
+      var source = new zb.ui.DataList(res);
+      self._exported.list.setSource(source);
+    })
+    .catch(function(xhr) {
+      console.error(xhr);
+    })
+
 };
 goog.inherits(SmartTv.scenes.Home, zb.layers.CuteScene);
 
@@ -25,3 +47,7 @@ SmartTv.scenes.Home.prototype._renderTemplate = function() {
  * @protected
  */
 SmartTv.scenes.Home.prototype._templateResult;
+
+zb.ui.BaseList.prototype.mouseClick = function(data) {
+  alert('click');
+};
