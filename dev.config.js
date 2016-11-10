5d446ca2f6b78dea4f611a7f259fa4b94e1580c7
borsch.watcher <borsch.watcher@gmail.com>
2016-11-10 04:27:22 +0200

    Update config for webos package

--- dev.config.js
+++ dev.config.js
@@ -8,8 +8,8 @@ module.exports = function(config) {
     webos: {
       'ares-package': '/usr/local/share/webOS_TV_SDK/CLI/bin/ares-package',
       'appinfo': {
-        'id': 'ru.esterdigital.smarttv',
-        'version': '1.0.0',
+        'id': 'co.ester.smarttv',
+        'version': '0.1.0',
         'vendor': 'Ester Digital',
         'type': 'web',
         'main': 'index.html',
